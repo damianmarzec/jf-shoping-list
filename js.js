27f46b7a95a2ed9db2c4060070f4071486e6da65
Damian Marzec <damian@MacBook-Pro-Damian.local>
2019-02-26 21:50:20 +0100

    Live code from lesson - init

---
+++ js.js
@@ -0,0 +1,34 @@
+
+function addNewItem(itemName) {
+  
+  // Create var with string (all element html)
+  let elementHtml = 
+    '<div class="list-element"> ' +
+      '<div class="text">' +
+        itemName +
+      '</div>' +
+      '<div class="icons">' +
+        '<div class="done">' +
+          '<input type="checkbox" >'+
+        '</div>' +
+        '<div class="delete">X</div>' +
+      '</div>' +
+    '</div>';
+
+  // Append created string to end of .list div html
+  // @todo make it ID not class
+  $('.list').append(elementHtml);
+
+  // add delete event listener
+  // We need to do it every time we add new div .delete
+  $('.delete').click(function () {
+    $(this).parent().parent().remove();
+  });
+}
+
+// document is ready
+$(function () {
+  $('#add-button').click(function () {
+    addNewItem($('#new-item').val());
+  });
+});
